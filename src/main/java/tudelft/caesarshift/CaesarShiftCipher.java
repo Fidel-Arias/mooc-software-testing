95214b9557edd6900c5c75666907d1bed9969d37
Fidel-Arias <fr06arias16@gmail.com>
2025-03-28 23:23:19 -0500

    Refactor(CaesarShiftCipher.java): Corrección de la lógica dentro del método CaesarShiftCipher.
    Se realizó la correción del nombre del método, luego se implementó una nueva condicional para validar los espacios en blanco y se elimino el código (sb.append(currentChar);) ubicado con anterioridad en la línea 15.

--- src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
+++ src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
@@ -2,7 +2,7 @@ package tudelft.caesarshift;
 
 public class CaesarShiftCipher {
 
-    public String CaesarShiftCipher(String message, int shift){
+    public String CaesarShiftCipherMethod(String message, int shift){
         StringBuilder sb = new StringBuilder();
         char currentChar;
         int length = message.length();
@@ -11,16 +11,19 @@ public class CaesarShiftCipher {
 
         for(int i = 0; i < length; i++){
             currentChar = message.charAt(i);
-           
-            sb.append(currentChar);
-            if (currentChar > 'z' || currentChar < 'a') {
+
+            if (currentChar == ' ') {
+                sb.append(' ');
+            } else if (currentChar > 'z' || currentChar < 'a') {
                 return "invalid";
-            } else if ((char) (currentChar + shift) > 'z') {
-                currentChar = (char) (currentChar - 26);
-            } else if ((char) (currentChar + shift) < 'a'){
-                currentChar = (char) (currentChar + 26);
+            } else {
+                if ((char) (currentChar + shift) > 'z') {
+                    currentChar = (char) (currentChar - 26);
+                } else if ((char) (currentChar + shift) < 'a') {
+                    currentChar = (char) (currentChar + 26);
+                }
+                sb.append((char) (currentChar + shift));
             }
-            sb.append((char) (currentChar + shift));
         }
 
         return sb.toString();
