59285d97ae839845b350e756bf67d5ffff2de21c
Fidel-Arias <fr06arias16@gmail.com>
2025-03-28 23:24:33 -0500

    Refactor(CaesarShiftCipherTest): Correción del nombre del método CaesarShiftCipher a CaesarShiftCipherMethod

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,8 +1,6 @@
 package tudelft.caesarshift;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CaesarShiftCipherTest {
@@ -10,38 +8,38 @@ public class CaesarShiftCipherTest {
     @Test
     public void ValidMessageWithPositiveShift() {
         CaesarShiftCipher cipher = new CaesarShiftCipher();
-        assertEquals("def", cipher.CaesarShiftCipher("abc", 3));
-        assertEquals("abc", cipher.CaesarShiftCipher("xyz", 3));
-        assertEquals("khoor", cipher.CaesarShiftCipher("hello", 3));
+        assertEquals("def", cipher.CaesarShiftCipherMethod("abc", 3));
+        assertEquals("abc", cipher.CaesarShiftCipherMethod("xyz", 3));
+        assertEquals("khoor", cipher.CaesarShiftCipherMethod("hello", 3));
     }
 
     @Test
     public void ValidMessageWithNegativeShift() {
         CaesarShiftCipher cipher = new CaesarShiftCipher();
-        assertEquals("xyz", cipher.CaesarShiftCipher("abc", -3));
-        assertEquals("abc", cipher.CaesarShiftCipher("def", -3));
-        assertEquals("ebiil", cipher.CaesarShiftCipher("hello", -3));
+        assertEquals("xyz", cipher.CaesarShiftCipherMethod("abc", -3));
+        assertEquals("abc", cipher.CaesarShiftCipherMethod("def", -3));
+        assertEquals("ebiil", cipher.CaesarShiftCipherMethod("hello", -3));
     }
 
     @Test
     public void testWithSpaces() {
         CaesarShiftCipher cipher = new CaesarShiftCipher();
-        assertEquals("d e f", cipher.CaesarShiftCipher("a b c", 3));
-        assertEquals("khoor zruog", cipher.CaesarShiftCipher("hello world", 3));
+        assertEquals("d e f", cipher.CaesarShiftCipherMethod("a b c", 3));
+        assertEquals("khoor zruog", cipher.CaesarShiftCipherMethod("hello world", 3));
     }
 
     @Test
     public void testInvalidCharacters() {
         CaesarShiftCipher cipher = new CaesarShiftCipher();
-        assertEquals("invalid", cipher.CaesarShiftCipher("abc123", 3));
-        assertEquals("invalid", cipher.CaesarShiftCipher("ABC", 3));
-        assertEquals("invalid", cipher.CaesarShiftCipher("a.b", 3));
+        assertEquals("invalid", cipher.CaesarShiftCipherMethod("abc123", 3));
+        assertEquals("invalid", cipher.CaesarShiftCipherMethod("ABC", 3));
+        assertEquals("invalid", cipher.CaesarShiftCipherMethod("a.b", 3));
     }
 
     @Test
     public void testLargeShift() {
         CaesarShiftCipher cipher = new CaesarShiftCipher();
-        assertEquals("abc", cipher.CaesarShiftCipher("abc", 26));  // Debe seguir igual
-        assertEquals("nop", cipher.CaesarShiftCipher("abc", 13));
+        assertEquals("abc", cipher.CaesarShiftCipherMethod("abc", 26));  // Debe seguir igual
+        assertEquals("nop", cipher.CaesarShiftCipherMethod("abc", 13));
     }
 }
