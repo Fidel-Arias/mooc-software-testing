5c086bee3c6b65f46ac117269f4eab253ffa04bc
Fidel-Arias <fr06arias16@gmail.com>
2025-05-01 09:35:45 -0500

    Test: Todos los metodos de la clase MirrorTest.java no presentan fallos las pruebas pasan

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,38 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MirrorTest {
+
+    @Test
+    public void testMirrorEnds1() {
+        Mirror m = new Mirror();
+        assertEquals("ab", m.mirrorEnds("abXYZba"));
+    }
+
+    @Test
+    public void testMirrorEnds2() {
+        Mirror m = new Mirror();
+        assertEquals("a", m.mirrorEnds("abca"));
+    }
+
+    @Test
+    public void testMirrorEnds3() {
+        Mirror m = new Mirror();
+        assertEquals("aba", m.mirrorEnds("aba"));
+    }
+
+    @Test
+    public void testMirrorEndsEmpty() {
+        Mirror m = new Mirror();
+        assertEquals("", m.mirrorEnds(""));
+    }
+
+    @Test
+    public void testMirrorEndsNoMatch() {
+        Mirror m = new Mirror();
+        assertEquals("", m.mirrorEnds("xyz"));
+    }
 }
+
